57593d16bc36a6a06ccf9537f6639e40b109161d
Sunil <snilavarath@gmail.com>
2022-01-26 11:21:44 -0500

    Done.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks }) {
+
+  const [taskList, setTaskList] = useState(tasks)
+  
+  function handleRemove(text){
+    const newList = taskList.filter((task) => task.text !== text);
+
+    setTaskList(newList);
+  }
+
+
+
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {taskList.map((task)=>(
+      <Task key={task.text}text={task.text} category={task.category} remove={handleRemove}/>
+      ))}
     </div>
   );
 }
