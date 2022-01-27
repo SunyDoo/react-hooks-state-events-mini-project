3ef13447842b76d61a2540fd634c4c937b572754
Sunil <snilavarath@gmail.com>
2022-01-27 10:06:46 -0500

    Done.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,11 +2,10 @@ import React from "react";
 import Task from "./Task";
 
 function TaskList({ tasks, onRemove }) {
-  const displayTasks = tasks
   
   return (
     <div className="tasks">
-      {displayTasks.map((task)=>(
+      {tasks.map((task)=>(
       <Task key={task.text}text={task.text} category={task.category} remove={onRemove}/>
       ))}
     </div>
